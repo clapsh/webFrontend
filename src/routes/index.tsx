76d4ce54a1b9b7fc68678f691f0cf8a05cdfed99
clapsh <psh8295015@gmail.com>
2026-08-12 10:04:46 +0900

    10:04

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -43,7 +43,7 @@ const router = createBrowserRouter([
       },
       {
         path: '/movies',
-        loader: requiresAuth,
+        //loader: requiresAuth,
         element: <Movies />,
         children: [
           {
--- src/routes/pages/Movies.single.tsx
+++ src/routes/pages/Movies.single.tsx
@@ -1,13 +1,9 @@
 import axios from 'axios'
-import { useState, useEffect } from 'react'
-import {
-  useInfiniteQuery,
-  //useQueryClient,
-  infiniteQueryOptions
-} from '@tanstack/react-query'
+import { useState } from 'react'
+import { useQuery, useQueryClient, queryOptions } from '@tanstack/react-query'
 import { Link, Outlet } from 'react-router'
 import { useMovieStore } from '../../stores/movies'
-import { useInView } from 'react-intersection-observer'
+
 export interface ResponseDataSuccess {
   Response: 'True'
   Search: Movie[]
@@ -27,43 +23,30 @@ export interface Movie {
 }
 
 export default function Movies() {
-  //const queryClient = useQueryClient()
+  const queryClient = useQueryClient()
   // const { searchText, setSearchText } = useMovieStore(s => s) // ❌ 잘못된 코드!
   const searchText = useMovieStore(s => s.searchText)
   const setSearchText = useMovieStore(s => s.setSearchText)
   const [inputText, setInputText] = useState(searchText)
-  const { ref, inView } = useInView()
-  const options = infiniteQueryOptions({
+  const options = queryOptions({
     queryKey: ['movies', searchText],
-    queryFn: async ({ pageParam }) => {
+    queryFn: async () => {
       // await new Promise(resolve => setTimeout(resolve, 3000))
       const { data } = await axios.post<ResponseData>('/api/movie', {
-        title: searchText,
-        page: pageParam
+        title: searchText
       })
-      if (data.Response === 'False') throw new Error(data.Error)
-      return data
+      const movies = data.Response === 'True' ? data.Search : []
+      return movies
     },
     staleTime: 1000 * 60 * 60 * 24, // 캐싱하는 시간(ms)
     enabled: Boolean(searchText),
-    placeholderData: prev => prev, // 깜빡이는 부분에 채워넣을 데이터
-    getNextPageParam: (lastPage, pages) => {
-      const maxPage = Math.ceil(Number(lastPage.totalResults) / 10)
-      const currentPage = pages.length
-      if (currentPage < maxPage) return currentPage + 1
-      return undefined
-    },
-
-    initialPageParam: 1
+    // select: data => {
+    //   const movies = data.Response === 'True' ? data.Search : []
+    //   return movies.filter(movie => Number(movie.Year) <= 2015)
+    // },
+    placeholderData: prev => prev // 깜빡이는 부분에 채워넣을 데이터
   })
-  const { data, fetchNextPage, isFetching, hasNextPage } =
-    useInfiniteQuery(options)
-
-  useEffect(() => {
-    if (inView) {
-      fetchNextPage()
-    }
-  }, [inView, fetchNextPage])
+  const { data: movies, refetch } = useQuery(options)
 
   function fetchMovies() {
     setSearchText(inputText)
@@ -83,19 +66,14 @@ export default function Movies() {
         <button onClick={() => fetchMovies()}>검색!</button>
       </div>
       <div>
+        <button onClick={() => refetch()}>다시 가져오기!(무조건)</button>
+        <button onClick={() => queryClient.fetchQuery(options)}>
+          다시 가져오기!(신선도에 따라)
+        </button>
+      </div>
+      <div>
         <ul>
-          {data?.pages.map(page => {
-            return page.Search.map(movie => {
-              return (
-                <li key={movie.imdbID}>
-                  <Link to={`/movies/${movie.imdbID}`}>
-                    {movie.Title}({movie.Year})
-                  </Link>
-                </li>
-              )
-            })
-          })}
-          {/* {movies?.map(movie => {
+          {movies?.map(movie => {
             return (
               <li key={movie.imdbID}>
                 <Link to={`/movies/${movie.imdbID}`}>
@@ -103,16 +81,8 @@ export default function Movies() {
                 </Link>
               </li>
             )
-          })} */}
+          })}
         </ul>
-        <button
-          ref={ref}
-          style={{
-            display: isFetching || !hasNextPage ? 'none' : 'block'
-          }}
-          onClick={() => fetchNextPage()}>
-          더보기
-        </button>
       </div>
       <Outlet />
     </>
